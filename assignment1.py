bea6c0d06c3a45b70ac714752332152942219964
Jyoti1010yadav <113248661+Jyoti1010yadav@users.noreply.github.com>
2022-09-11 02:08:39 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,64 @@
+'''question_1
+Write a command to get the Python version you are using. 
+'''
+# we should write python --version 
+
+'''question_2
+2. Write a python script to print Hello Python on the screen directly in the command
+line.
+'''
+# we should write "Hello Python"  Note:- Dont forget to use double/single quotes while writing.
+
+'''question_3
+Write a python script and store it in a file and execute the file from the command line
+
+solution:-
+
+we should first open python idle and then go to new file and 
+write our code in that then save that file with .py exetension and then click on run
+''' 
+
+'''Question_4
+Write a python script to print Hello Python on the screen using visual studio code.
+'''
+print("Hello Python")
+
+'''Question_5
+5. Write a python script to print Hello on the first line and Python on the second line.
+'''
+print("Hello","Python",sep="\n")
+
+'''question_6
+Write a python script to print “MySirG” on the screen
+'''
+print('"MySirG"')
+
+'''question_7
+Write a python script to print “Teacher’s Day” on the screen
+'''
+print(''' “Teacher’s Day" ''')
+
+'''question_8
+Write a python script to print the value of a variable. Variable contains your name as
+data.'''
+Name="Jyoti yadav"
+print(Name)
+
+'''question_9
+Write a python script to create variables to store your name, age, qualification, years
+of experience and print it.
+'''
+name="Jyoti Yadav "
+age=20
+qualification="Btech_3rd_year"
+years_of_experience='0 years'
+print('name=',name,'age=',age,'qualification=',qualification,'years_of_experience=',years_of_experience,sep=" ")
+
+'''question_10
+Write a python script to print values of three variables, each in a new line. Variable
+contains integer values
+'''
+var1=12
+var2=24
+var3=36
+print(var1,var2,var3,sep="\n")
